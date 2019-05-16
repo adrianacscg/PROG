9cf2016fa48218d719cf5540da3ec0b9767f0d66
Tohgo <43523027+SnarkyTohgo@users.noreply.github.com>
2019-05-16 19:56:17 +0100

    Nova classe date.h

--- header_files/date.h
+++ header_files/date.h
@@ -3,8 +3,40 @@
 
 using namespace std;
 
-struct Date {
-    int dia;
-    int mes;
-    int ano;
+class Date {
+    private:
+        int dia, mes, ano;
+
+    public:
+        // @Constructors
+        Date();
+        Date(int d, int m, int a);
+
+
+        // @Get Methods
+        const int getDia();
+        const int getMes();
+        const int getAno();
+
+        // @Set Methods
+        void setDia(int d);
+        void setMes(int m);
+        void setAno(int a);
+
+        // @Operators
+        const bool operator < (Date d) {
+                if (this->ano < d.ano)return true;
+                if (this->ano == d.ano){
+                        if (this->mes < d.mes)return true;
+                        if (this->mes == d.mes){
+                                if (dia < d.dia)return true;
+                        }
+                }
+                return false;
+        }
+        const bool operator == (Date d){
+            if (dia == d.dia && mes == d.mes && ano == d.ano)return true;
+            else return false;
+        }
 };
+
