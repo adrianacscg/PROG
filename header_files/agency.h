92caea285517177c33f352f667add35d6e8e3d93
Adriana <43761025+adrianacscg@users.noreply.github.com>
2019-05-17 19:12:37 +0100

    Criação da classe Agency

--- header_files/agency.h
+++ header_files/agency.h
@@ -3,11 +3,36 @@
 
 using namespace std;
 
-struct Agency {
-    string nome;
-    int nif;
-    Address morada;
-    string url;
-    vector<TravelPack> pacotes;
-    vector<Client> clientes;
+
+class Agency {
+private:
+	string nome;
+	int nif;
+	Address morada;
+	string url;
+	vector<TravelPack> pacotes;
+	vector<Client> clientes;
+
+public:
+
+	Agency();
+	Agency(string name, int vat, Address address, string link, vector<TravelPack> packs, vector<Client> clients);
+
+	//GET
+	string getNome() const;
+	int getNIF() const;
+	Address getMorada() const;
+	string getURL() const;
+	vector<TravelPack> getPacotes() const;
+	vector<Client> getClientes() const;
+
+	//SET
+
+	void setNome(string name);
+	void setNIF(int vat);
+	void setMorada(Address address);
+	void setURL(string link);
+	void setPacotes(vector<TravelPack> packs);
+	void setClientes(vector<Client> clients);
 };
+
